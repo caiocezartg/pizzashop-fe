17c429854189c886818c175e9322c47e759337d5
Caio Cezar <caiocezartg@gmail.com>
2024-02-14 11:45:14 -0300

    feat: add success variant to button component

---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,30 @@
+import { Button } from './ui/button'
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog'
+
+export default function StoreProfileDialog() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <DialogFooter>
+        <Button type="button" variant="ghost">
+          Cancelar
+        </Button>
+        <Button type="submit" variant="success">
+          Salvar
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  )
+}
